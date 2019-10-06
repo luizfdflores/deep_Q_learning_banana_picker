84da33bb9e0116acafbbb6312bd77631c7ac0581
Luiz Felipe Diez Flores <luizfdflores@gmail.com>
2019-10-06 11:03:50 -0300

    Update model.py, removing previous commented rows.

--- model.py
+++ model.py
@@ -19,22 +19,11 @@ class Network(nn.Module):
             out_features = action_size*(2**(n_fc_layers-i))
             modules.append(nn.Linear(in_features, out_features))
             modules.append(nn.ReLU())
-        
+
         modules.append(nn.Linear(action_size*2, action_size))
-          
+
         self.fc_layers = nn.Sequential(*modules)
-            
-        #self.fc1 = nn.Linear(state_size, action_size*16)
-        #self.fc2 = nn.Linear(action_size*16, action_size*8)
-        #self.fc3 = nn.Linear(action_size*8, action_size*4)
-        #self.fc4 = nn.Linear(action_size*4, action_size*2)
-        #self.fc5 = nn.Linear(action_size*2, action_size)
 
     def forward(self, state):
         """Build a network that maps state -> action values."""
-        #x = F.relu(self.fc1(state))
-        #x = F.relu(self.fc2(x))
-        #x = F.relu(self.fc3(x))
-        #x = F.relu(self.fc4(x))
-        #return self.fc5(x)
         return self.fc_layers(state)
